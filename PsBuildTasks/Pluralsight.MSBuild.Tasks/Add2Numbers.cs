2e65b5e5c32178a28b230695e15467f4eed934f0
aditya <aditya.kaki@oracle.com>
2020-02-28 16:03:29 +0530

    Added files to repo

---
+++ PsBuildTasks/Pluralsight.MSBuild.Tasks/Add2Numbers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Pluralsight.MSBuild.Tasks
+{
+    public class Add2Numbers : ITask
+    {
+        [Required]
+        public double Number1 { get; set; }
+        
+        [Required]
+        public double Number2 { get; set; }
+
+        [Output]
+        public double Result { get; set; }
+
+        public bool Execute()
+        {
+            Result = Number1 + Number2;
+
+            BuildEngine.LogMessageEvent(new BuildMessageEventArgs("Added 2 numbers", "Math", "Add2Numbers", MessageImportance.High));
+
+            return true;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+
+        public ITaskHost HostObject { get; set; }
+    }
+}
---
+++ PsBuildTasks/Pluralsight.MSBuild.Tasks/Multiply2Numbers.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Pluralsight.MSBuild.Tasks
+{
+    public class Multiply2Numbers : Task
+    {
+        [Required]
+        public double Number1 { get; set; }
+
+        [Required]
+        public double Number2 { get; set; }
+
+        [Output]
+        public double Result { get; set; }
+
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Multiplied 2 numbers", null);
+
+            Result = Number1 * Number2;
+
+            return true;
+        }
+    }
+}
